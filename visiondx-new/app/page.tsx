0ec21e2c2701a3ab6fa6c7bcaa1f1ef736048872
aarushrastogi123 <aarushrastogi123@users.noreply.github.com>
2026-08-24 22:02:46 +0530

    .

--- visiondx-new/app/page.tsx
+++ visiondx-new/app/page.tsx
@@ -27,9 +27,9 @@ export default async function Home() {
 
           {/* Logo */}
           <div className="flex items-center gap-3">
-            <span className="text-3xl font-bold tracking-tight text-white">
+            <Link href="https://visiondx.vercel.app" className="text-3xl font-bold tracking-tight text-white">
               Vision<span className="text-cyan-300">DX</span>
-            </span>
+            </Link>
           </div>
 
           {/* Navigation */}
